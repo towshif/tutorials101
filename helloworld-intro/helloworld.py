54b389655fc3f184cb918f9535426f32c9ac7cfb
towshif <towshif@gmail.com>
2018-11-24 05:22:47 +0000

    new

--- helloworld-intro/helloworld.py
+++ helloworld-intro/helloworld.py
@@ -3,5 +3,15 @@ def addme(a, b):
     return a+b
 
 
+def multme(a, b):
+    return a*b
+
+
+def divideme(a, b):
+    return a/b
+
+
 print("Hello World")
 print("Add 5 + 2 = ", addme(5, 2))
+print("Multiply 5 * 2 = ", multme(5, 2))
+print("Divide 5 / 2 = ", divideme(5, 2))
